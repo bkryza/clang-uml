d62a2a1e9ffc198dd81dafbf126e618adbb9f1df
Bartek Kryza <bkryza@gmail.com>
2022-01-16 18:34:15 +0100

    Updated copyright dates

--- src/class_diagram/model/enums.h
+++ src/class_diagram/model/enums.h
@@ -1,7 +1,7 @@
 /**
  * src/class_diagram/model/enums.h
  *
- * Copyright (c) 2021 Bartek Kryza <bkryza@gmail.com>
+ * Copyright (c) 2021-2022 Bartek Kryza <bkryza@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
