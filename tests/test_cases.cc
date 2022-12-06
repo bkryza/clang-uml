de4e88a92d1a35b9263d56f6d3f7b1a53d8eb5bb
Bartek Kryza <bkryza@gmail.com>
2022-12-06 22:31:54 +0100

    Added class exclusion by namespace in sequence diagram test case

--- tests/test_cases.cc
+++ tests/test_cases.cc
@@ -261,6 +261,7 @@ using namespace clanguml::test::matchers;
 #include "t20012/test_case.h"
 #include "t20013/test_case.h"
 #include "t20014/test_case.h"
+#include "t20015/test_case.h"
 
 ///
 /// Package diagram tests
