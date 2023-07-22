ec0079d8761f9978898b1dc9cca431bd943e32cd
Bartek Kryza <bkryza@gmail.com>
2023-07-23 01:22:14 +0200

    Added include/exclude filter specification to validator schema

--- src/config/schema.h
+++ src/config/schema.h
@@ -24,14 +24,30 @@ namespace clanguml::config {
 const std::string schema_str = R"(
 types:
     map_t<K;V>: { $K: V }
-    comment_parser_t: !variant [plain, clang]
-    diagram_type_t: !variant [class, sequence, include, package]
-    generate_method_arguments_t: !variant [full, abbreviated, none]
+    comment_parser_t: !variant
+        - plain
+        - clang
+    diagram_type_t: !variant
+        - class
+        - sequence
+        - include
+        - package
+    generate_method_arguments_t: !variant
+        - full
+        - abbreviated
+        - none
     generate_links_t:
         link: string
         tooltip: string
     git_t: map_t<string;string>
-    layout_hint_key: !variant [up, left, right, down, row, column, together]
+    layout_hint_key: !variant
+        - up
+        - left
+        - right
+        - down
+        - row
+        - column
+        - together
     layout_hint_value: [string, [string]]
     layout_hint_t: [map_t<layout_hint_key;layout_hint_value>]
     layout_t: map_t<string;layout_hint_t>
@@ -40,8 +56,65 @@ types:
     regex_t:
         r: string
     regex_or_string_t: [string, regex_t]
-    namespaces_filter_t: regex_or_string_t
-    elements_filter_t: regex_or_string_t
+    namespaces_filter_t:
+        namespaces: [regex_or_string_t]
+    elements_filter_t:
+        elements: [regex_or_string_t]
+    element_types_filter_t: !variant
+        - class
+        - enum
+        - concept
+    relationship_filter_t: !variant
+        - extension
+        - inheritance
+        - composition
+        - aggregation
+        - containment
+        - ownership
+        - association
+        - instantiation
+        - friendship
+        - alias
+        - dependency
+        - constraint
+        - none
+    access_filter_t: !variant
+        - public
+        - protected
+        - private
+    method_type_filter_t: !variant
+        - constructor
+        - destructor
+        - assignment
+        - operator
+        - defaulted
+        - deleted
+        - static
+    callee_type_filter_t: !variant
+        - constructor
+        - assignment
+        - operator
+        - defaulted
+        - static
+        - method
+        - function
+        - function_template
+        - lambda
+    filter_t:
+        namespaces: !optional [regex_or_string_t]
+        elements: !optional [regex_or_string_t]
+        element_types: !optional [element_types_filter_t]
+        relationships: !optional [relationship_filter_t]
+        access: !optional [access_filter_t]
+        subclasses: !optional [regex_or_string_t]
+        parents: !optional [regex_or_string_t]
+        specializations: !optional [regex_or_string_t]
+        dependants: !optional [regex_or_string_t]
+        dependencies: !optional [regex_or_string_t]
+        context: !optional [regex_or_string_t]
+        paths: !optional [string]
+        method_types: !optional [method_type_filter_t]
+        callee_types: !optional [callee_type_filter_t]
     function_location_t:
         function: string
     marker_location_t:
@@ -55,14 +128,13 @@ types:
         # Common options
         #
         __parent_path: !optional string
-        base_directory: !optional string
         comment_parser: !optional comment_parser_t
         debug_mode: !optional bool
-        exclude: !optional map_t<string;any>
+        exclude: !optional filter_t
         generate_links: !optional generate_links_t
         git: !optional git_t
         glob: !optional [string]
-        include: !optional map_t<string;any>
+        include: !optional filter_t
         plantuml: !optional
             before: !optional [string]
             after: !optional [string]
@@ -88,14 +160,13 @@ types:
         # Common options
         #
         __parent_path: !optional string
-        base_directory: !optional string
         comment_parser: !optional comment_parser_t
         debug_mode: !optional bool
-        exclude: !optional map_t<string;any>
+        exclude: !optional filter_t
         generate_links: !optional generate_links_t
         git: !optional git_t
         glob: !optional [string]
-        include: !optional map_t<string;any>
+        include: !optional filter_t
         plantuml: !optional
             before: !optional [string]
             after: !optional [string]
@@ -117,14 +188,13 @@ types:
         # Common options
         #
         __parent_path: !optional string
-        base_directory: !optional string
         comment_parser: !optional comment_parser_t
         debug_mode: !optional bool
-        exclude: !optional map_t<string;any>
+        exclude: !optional filter_t
         generate_links: !optional generate_links_t
         git: !optional git_t
         glob: !optional [string]
-        include: !optional map_t<string;any>
+        include: !optional filter_t
         plantuml: !optional
             before: !optional [string]
             after: !optional [string]
@@ -143,14 +213,13 @@ types:
         # Common options
         #
         __parent_path: !optional string
-        base_directory: !optional string
         comment_parser: !optional comment_parser_t
         debug_mode: !optional bool
-        exclude: !optional map_t<string;any>
+        exclude: !optional filter_t
         generate_links: !optional generate_links_t
         git: !optional git_t
         glob: !optional [string]
-        include: !optional map_t<string;any>
+        include: !optional filter_t
         plantuml: !optional
             before: !optional [string]
             after: !optional [string]
@@ -186,14 +255,13 @@ root:
     # Common options
     #
     __parent_path: !optional string
-    base_directory: !optional string
     comment_parser: !optional comment_parser_t
     debug_mode: !optional bool
-    exclude: !optional map_t<string;any>
+    exclude: !optional filter_t
     generate_links: !optional generate_links_t
     git: !optional git_t
     glob: !optional [string]
-    include: !optional map_t<string;any>
+    include: !optional filter_t
     plantuml: !optional
         before: !optional [string]
         after: !optional [string]
