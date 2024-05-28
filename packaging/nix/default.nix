57a681a6262cc529de3a5b1ec52e52d346a4003d
Pogobanane <aenderboy@gmx.de>
2024-05-28 12:18:08 +0000

    nix package: add wrapper from nixpkgs#clang-tools

--- packaging/nix/default.nix
+++ packaging/nix/default.nix
@@ -4,12 +4,19 @@
   pkg-config,
   installShellFiles,
   libclang,
+  clang,
+  llvmPackages,
   libllvm,
   yaml-cpp,
+  enableLibcxx ? false,
 }:
 stdenv.mkDerivation {
   name = "clang-uml";
   src = ../..;
+
+  # variables for substituteAll
+  unwrapped = llvmPackages.clang-unwrapped;
+  clang = if enableLibcxx then llvmPackages.libcxxClang else llvmPackages.clang;
 
   nativeBuildInputs = [
     cmake
@@ -18,12 +25,14 @@ stdenv.mkDerivation {
   ];
 
   buildInputs = [
+    clang
     libclang
     libllvm
     yaml-cpp
   ];
 
   postInstall = ''
+    substituteAll ${./wrapper} $out/bin/clang-uml-wrapped
     installShellCompletion --bash $src/packaging/autocomplete/clang-uml
     installShellCompletion --zsh $src/packaging/autocomplete/_clang-uml
   '';
