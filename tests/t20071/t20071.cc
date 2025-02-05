66038482f5026e2de5d0d94468cfa4ddc9a313e8
Bartek Kryza <bkryza@gmail.com>
2025-02-05 13:22:47 +0100

    Updated coroutine test case with local class (#384)

--- tests/t20071/t20071.cc
+++ tests/t20071/t20071.cc
@@ -4,21 +4,22 @@
 #include <thread>
 
 namespace clanguml::t20071 {
-struct awaitable_on_thread {
-    std::thread *p_out;
-    bool await_ready() { return false; }
-    void await_suspend(std::coroutine_handle<> h)
-    {
-        auto &out = *p_out;
-        if (out.joinable())
-            throw std::runtime_error("Output thread parameter not empty");
-        out = std::thread([h] { h.resume(); });
-    }
-    void await_resume() { }
-};
 
 auto switch_to_new_thread(std::thread &out)
 {
+    struct awaitable_on_thread {
+        std::thread *p_out;
+        bool await_ready() { return false; }
+        void await_suspend(std::coroutine_handle<> h)
+        {
+            auto &out = *p_out;
+            if (out.joinable())
+                throw std::runtime_error("Output thread parameter not empty");
+            out = std::thread([h] { h.resume(); });
+        }
+        void await_resume() { }
+    };
+
     return awaitable_on_thread{&out};
 }
 
